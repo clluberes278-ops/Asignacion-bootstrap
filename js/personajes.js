622f744e9326f93aebdef8f2cb3c62dadc92b6aa
clluberes278-ops <clluberes278@gmail.com>
2026-06-24 14:58:43 -0400

    comenzando js

---
+++ js/personajes.js
@@ -0,0 +1,57 @@
+const BD_agentes = {
+    "ellen": {
+        nombre: "Ellen Joe",
+        imagen: "",
+        atributo: "",
+        rol:"",
+        descripcion: "",
+        buildPremium:"",
+        buildF2P:"",
+        sinergia: "",
+    },
+
+    "Agent 0 Anby": {
+        nombre: "Ellen Joe",
+        imagen: "",
+        atributo: "",
+        rol:"",
+        descripcion: "",
+        buildPremium:"",
+        buildF2P:"",
+        sinergia: "",
+    },
+
+    "Agent 0 Anby": {
+        nombre: "Ellen Joe",
+        imagen: "",
+        atributo: "",
+        rol:"",
+        descripcion: "",
+        buildPremium:"",
+        buildF2P:"",
+        sinergia: "",
+    },
+
+    "Agent 0 Anby": {
+        nombre: "Ellen Joe",
+        imagen: "",
+        atributo: "",
+        rol:"",
+        descripcion: "",
+        buildPremium:"",
+        buildF2P:"",
+        sinergia: "",
+    },
+
+    "Agent 0 Anby": {
+        nombre: "Ellen Joe",
+        imagen: "",
+        atributo: "",
+        rol:"",
+        descripcion: "",
+        buildPremium:"",
+        buildF2P:"",
+        sinergia: "",
+    }
+
+}
