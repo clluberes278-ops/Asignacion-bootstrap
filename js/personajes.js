20bd4c707c19e2c5e69cba6faa24f57f90b15dd9
clluberes278-ops <clluberes278@gmail.com>
2026-06-25 19:00:09 -0400

    cambios con js

--- js/personajes.js
+++ js/personajes.js
@@ -1,57 +1,566 @@
 const BD_agentes = {
-    "ellen": {
-        nombre: "Ellen Joe",
+    // ------------------- LIEBRES ASTUTAS -------------------
+    "Nekomata": {
+        nombre: "Nekomiya Mana",
         imagen: "",
-        atributo: "",
-        rol:"",
+        atributo: "Fisico",
+        especificacion: "Atacante",
         descripcion: "",
-        buildPremium:"",
-        buildF2P:"",
-        sinergia: "",
+        buildpremium: "",
+        buildF2P: "",
+        sinergias: "",
+    },
+    "Anby": {
+        nombre: "Anby Demara",
+        imagen: "",
+        atributo: "Electrico",
+        especificacion: "Aturdidor",
+        descripcion: "",
+        buildpremium: "",
+        buildF2P: "",
+        sinergias: "",
+    },
+    "Nicole": {
+        nombre: "Nicole Demara",
+        imagen: "",
+        atributo: "Eter",
+        especificacion: "Auxiliar",
+        descripcion: "",
+        buildpremium: "",
+        buildF2P: "",
+        sinergias: "",
+    },
+    "Billy": {
+        nombre: "Billy Kid",
+        imagen: "",
+        atributo: "Fisico",
+        especificacion: "Atacante",
+        descripcion: "",
+        buildpremium: "",
+        buildF2P: "",
+        sinergias: "",
     },
 
-    "Agent 0 Anby": {
-        nombre: "Ellen Joe",
+    "Starlight Billy": {
+        nombre: "Starlight Billy",
         imagen: "",
-        atributo: "",
-        rol:"",
+        atributo: "Fisico",
+        especificacion: "Atacante",
         descripcion: "",
-        buildPremium:"",
-        buildF2P:"",
-        sinergia: "",
+        buildpremium: "",
+        buildF2P: "",
+        sinergias: "",
     },
 
-    "Agent 0 Anby": {
+    // ------------------- SERVICIOS DOMÉSTICOS VICTORIA -------------------
+    "Ellen": {
         nombre: "Ellen Joe",
         imagen: "",
-        atributo: "",
-        rol:"",
+        atributo: "Hielo",
+        especificacion: "Atacante",
         descripcion: "",
-        buildPremium:"",
-        buildF2P:"",
-        sinergia: "",
+        buildpremium: "",
+        buildF2P: "",
+        sinergias: "",
+    },
+    "Lycaon": {
+        nombre: "Von Lycaon",
+        imagen: "",
+        atributo: "Hielo",
+        especificacion: "Aturdidor",
+        descripcion: "",
+        buildpremium: "",
+        buildF2P: "",
+        sinergias: "",
+    },
+    "Rina": {
+        nombre: "Alexandrina Sebastiane",
+        imagen: "",
+        atributo: "Electrico",
+        especificacion: "Auxiliar",
+        descripcion: "",
+        buildpremium: "",
+        buildF2P: "",
+        sinergias: "",
+    },
+    "Corin Wickes": {
+        nombre: "Corin Wickes", 
+        imagen: "",
+        atributo: "Fisico",
+        especificacion: "Atacante",
+        descripcion: "",
+        buildpremium: "",
+        buildF2P: "",
+        sinergias: "",
     },
 
-    "Agent 0 Anby": {
-        nombre: "Ellen Joe",
+    // ------------------- INDUSTRIAS BELOBOG -------------------
+    "Grace": {
+        nombre: "Grace Howards",
         imagen: "",
-        atributo: "",
-        rol:"",
+        atributo: "Electrico",
+        especificacion: "Anomalo",
         descripcion: "",
-        buildPremium:"",
-        buildF2P:"",
-        sinergia: "",
+        buildpremium: "",
+        buildF2P: "",
+        sinergias: "",
+    },
+    "Koleda": {
+        nombre: "Koleda Belobog",
+        imagen: "",
+        atributo: "Fuego",
+        especificacion: "Aturdidor",
+        descripcion: "",
+        buildpremium: "",
+        buildF2P: "",
+        sinergias: "",
+    },
+    "Anton": {
+        nombre: "Anton Ivanov",
+        imagen: "",
+        atributo: "Electrico",
+        especificacion: "Atacante",
+        descripcion: "",
+        buildpremium: "",
+        buildF2P: "",
+        sinergias: "",
+    },
+    "Ben": {
+        nombre: "Ben Bigger",
+        imagen: "",
+        atributo: "Fuego",
+        especificacion: "Defensor",
+        descripcion: "",
+        buildpremium: "",
+        buildF2P: "",
+        sinergias: "",
     },
 
-    "Agent 0 Anby": {
-        nombre: "Ellen Joe",
+    // ------------------- DIVISIÓN N.º 6 -------------------
+    "Soukaku": {
+        nombre: "Soukaku",
+        imagen: "",
+        atributo: "Hielo",
+        especificacion: "Auxiliar",
+        descripcion: "",
+        buildpremium: "",
+        buildF2P: "",
+        sinergias: "",
+    },
+    "Yanagi": {
+        nombre: "Tsukishiro Yanagi",
+        imagen: "",
+        atributo: "Electrico",
+        especificacion: "Anomalo",
+        descripcion: "",
+        buildpremium: "",
+        buildF2P: "",
+        sinergias: "",
+    },
+    "Harumasa": {
+        nombre: "Harumasa",
+        imagen: "",
+        atributo: "Electrico",
+        especificacion: "Atacante",
+        descripcion: "",
+        buildpremium: "",
+        buildF2P: "",
+        sinergias: "",
+    },
+    "Miyabi": {
+        nombre: "Hoshimi Miyabi",
+        imagen: "",
+        atributo: "Hielo", 
+        especificacion: "Anomalo", 
+        descripcion: "",
+        buildpremium: "",
+        buildF2P: "",
+        sinergias: "",
+    },
+
+    // ------------------- NEPS (UNIDAD DE RESPUESTA ESPECIAL) -------------------
+    "Zhu Yuan": {
+        nombre: "Zhu Yuan",
+        imagen: "",
+        atributo: "Eter",
+        especificacion: "Atacante",
+        descripcion: "",
+        buildpremium: "",
+        buildF2P: "",
+        sinergias: "",
+    },
+    "Qingyi": {
+        nombre: "Qingyi",
+        imagen: "",
+        atributo: "Electrico",
+        especificacion: "Aturdidor",
+        descripcion: "",
+        buildpremium: "",
+        buildF2P: "",
+        sinergias: "",
+    },
+    "Seth": {
+        nombre: "Seth Lowell",
+        imagen: "",
+        atributo: "Electrico",
+        especificacion: "Defensor",
+        descripcion: "",
+        buildpremium: "",
+        buildF2P: "",
+        sinergias: "",
+    }, 
+    
+    "Jane Doe": {
+        nombre: "Jane Doe",
+        imagen: "",
+        atributo: "Fisico",
+        especificacion: "Anomalo",
+        descripcion: "",
+        buildpremium: "",
+        buildF2P: "",
+        sinergias: "",
+    },
+
+    // ------------------- HIJOS DE CALIDÓN -------------------
+    "Lucy": {
+        nombre: "Lucy",
+        imagen: "",
+        atributo: "Fuego",
+        especificacion: "Auxiliar",
+        descripcion: "",
+        buildpremium: "",
+        buildF2P: "",
+        sinergias: "",
+    },
+    "Piper": {
+        nombre: "Piper Wheel",
+        imagen: "",
+        atributo: "Fisico",
+        especificacion: "Anomalo",
+        descripcion: "",
+        buildpremium: "",
+        buildF2P: "",
+        sinergias: "",
+    },
+    "Burnice": {
+        nombre: "Burnice White",
+        imagen: "",
+        atributo: "Fuego",
+        especificacion: "Anomalo",
+        descripcion: "",
+        buildpremium: "",
+        buildF2P: "",
+        sinergias: "",
+    },
+    "Caesar": {
+        nombre: "Caesar King",
+        imagen: "",
+        atributo: "Fisico",
+        especificacion: "Defensor",
+        descripcion: "",
+        buildpremium: "",
+        buildF2P: "",
+        sinergias: "",
+    },
+    "Lighter": {
+        nombre: "Lighter",
+        imagen: "",
+        atributo: "Fuego",
+        especificacion: "Aturdidor",
+        descripcion: "",
+        buildpremium: "",
+        buildF2P: "",
+        sinergias: "",
+    },
+
+     "Pulchra": {
+        nombre: "Pulchra",
+        imagen: "",
+        atributo: "Fisico",
+        especificacion: "Aturdidor",
+        descripcion: "",
+        buildpremium: "",
+        buildF2P: "",
+        sinergias: "",
+    },
+
+    // ------------------- ESTRELLAS DE LIRA -------------------
+    "Astra Yao": {
+        nombre: "Astra Yao",
+        imagen: "",
+        atributo: "Eter",
+        especificacion: "Auxiliar",
+        descripcion: "",
+        buildpremium: "",
+        buildF2P: "",
+        sinergias: "",
+    },
+    "Evelyn": {
+        nombre: "Evelyn",
+        imagen: "",
+        atributo: "Fuego",
+        especificacion: "Atacante",
+        descripcion: "",
+        buildpremium: "",
+        buildF2P: "",
+        sinergias: "",
+    },
+
+    // ------------------- ESCUADRÓN OBOL -------------------
+    "Soldier 11": {
+        nombre: "Soldier 11",
+        imagen: "",
+        atributo: "Fuego",
+        especificacion: "Atacante",
+        descripcion: "",
+        buildpremium: "",
+        buildF2P: "",
+        sinergias: "",
+    },
+    "Trigger": {
+        nombre: "Trigger",
+        imagen: "",
+        atributo: "Electrico",
+        especificacion: "Aturdidor",
+        descripcion: "",
+        buildpremium: "",
+        buildF2P: "",
+        sinergias: "",
+    },
+    "Orphie y Magus":{
+        nombre:"Orphie y Magus",
+        imagen:"",
+        atributo:"Fuego",
+        especificacion:"Atacante",
+        descripcion:"",
+        buildpremium:"",
+        buildF2P:"",
+        sinergias:"",
+    },
+
+        // ------------------- SPOOK SHACK -------------------
+    "Ukinami Yuzuha": {
+        nombre: "Ukinami Yuzuha",
+        imagen: "",
+        atributo: "Fisico",
+        especificacion: "Auxiliar",
+        descripcion: "Creadora y fundadora del foro Spook Shack. Fue sujeto de pruebas del Proyecto New Leaf de Porcelumex.",
+        buildpremium: "",
+        buildF2P: "",
+        sinergias: "Yuzuha + Promeia + Velina | Yuzuha + Zhu Yuan + Nicole",
+    },
+    "Alice Thymefield": {
+        nombre: "Alice Thymefield",
         imagen: "",
         atributo: "",
-        rol:"",
-        descripcion: "",
-        buildPremium:"",
-        buildF2P:"",
-        sinergia: "",
+        especificacion: "",
+        descripcion: "Encargada de investigaciones de campo en Spook Shack. Heredera de la familia Thymefield.",
+        buildpremium: "",
+        buildF2P: "",
+        sinergias: "",
+    },
+    "Komano Manato": {
+        nombre: "Komano Manato",
+        imagen: "",
+        atributo: "",
+        especificacion: "",
+        descripcion: "Co-fundador y administrador del foro Spook Shack. Compañero de clase de Yuzuha.",
+        buildpremium: "",
+        buildF2P: "",
+        sinergias: "",
+    },
+    "Lucia Elowen": {
+        nombre: "Lucia Elowen",
+        imagen: "",
+        atributo: "",
+        especificacion: "",
+        descripcion: "Experta en bestias etéreas de Spook Shack.",
+        buildpremium: "",
+        buildF2P: "",
+        sinergias: "",
+    },
+    "Yidhari Murphy": {
+        nombre: "Yidhari Murphy",
+        imagen: "",
+        atributo: "",
+        especificacion: "",
+        descripcion: "Encargada de historias de terror y consultas de usuarios en Spook Shack.",
+        buildpremium: "",
+        buildF2P: "",
+        sinergias: "",
+    },
+
+    // ------------------- KRAMPUS COMPLIANCE AUTHORITY -------------------
+    "Promeia": {
+        nombre: "Promeia",
+        imagen: "",
+        atributo: "Hielo",
+        especificacion: "Anomalo",
+        descripcion: "Una de las Jueces de Krampus. Estoica y despiadada. Antigua Barrendera que se redimió como ejecutora.",
+        buildpremium: "",
+        buildF2P: "",
+        sinergias: "Promeia + Velina + Yuzuha | Promeia + Vivian + Aria",
+    },
+    "Zhao": {
+        nombre: "Zhao",
+        imagen: "",
+        atributo: "Hielo",
+        especificacion: "Defensor",
+        descripcion: "Miembro de Krampus. Considerada un 'desastre' por los empleados de empresas corruptas de TOPS.",
+        buildpremium: "",
+        buildF2P: "",
+        sinergias: "Zhao + Promeia + Yuzuha | Zhao + Ellen + Lycaon",
+    },
+    "Dialyn": {
+        nombre: "Dialyn",
+        imagen: "",
+        atributo: "Fisico",
+        especificacion: "Aturdidor",
+        descripcion: "Representante de atención al cliente de TOPS que se convirtió en Juez de Krampus.",
+        buildpremium: "",
+        buildF2P: "",
+        sinergias: "Dialyn + Promeia + Yuzuha | Dialyn + Zhu Yuan + Nicole",
+    },
+    "Banyue": {
+        nombre: "Banyue",
+        imagen: "",
+        atributo: "",
+        especificacion: "",
+        descripcion: "Constructo inteligente que dirige una escuela de artes marciales. Miembro de Krampus.",
+        buildpremium: "",
+        buildF2P: "",
+        sinergias: "",
+    },
+
+    // ------------------- YUNKUI SUMMIT -------------------
+    "Ye Shunguang": {
+        nombre: "Ye Shunguang",
+        imagen: "",
+        atributo: "",
+        especificacion: "",
+        descripcion: "Cazadora del Vacío: Qingming Arbiter. Portadora actual de la Espada Qingming. Miembro de Yunkui Summit.",
+        buildpremium: "",
+        buildF2P: "",
+        sinergias: "Ye Shunguang + Yixuan + Ju Fufu",
+    },
+    "Pan Yinhui": {
+        nombre: "Pan Yinhui",
+        imagen: "",
+        atributo: "",
+        especificacion: "",
+        descripcion: "Miembro de Yunkui Summit. Discípula del Templo Suibian.",
+        buildpremium: "",
+        buildF2P: "",
+        sinergias: "Pan Yinhui + Yixuan + Ju Fufu",
+    },
+    "Yixuan": {
+        nombre: "Yixuan",
+        imagen: "",
+        atributo: "",
+        especificacion: "",
+        descripcion: "Cazadora del Vacío. Suma Sacerdotisa de la 13.ª generación y líder de Yunkui Summit.",
+        buildpremium: "",
+        buildF2P: "",
+        sinergias: "Yixuan + Pan Yinhui + Ju Fufu | Yixuan + Ye Shunguang + Ju Fufu",
+    },
+    "Ju Fufu": {
+        nombre: "Ju Fufu",
+        imagen: "",
+        atributo: "Fuego",
+        especificacion: "",
+        descripcion: "La discípula de mayor rango de Yunkui Summit. Hermana mayor de Ye Shunguang.",
+        buildpremium: "",
+        buildF2P: "",
+        sinergias: "Ju Fufu + Yixuan + Pan Yinhui | Ju Fufu + Ye Shunguang + Yixuan",
+    },
+
+    // ------------------- ANGELS OF DELUSION -------------------
+    "Nangong Yu": {
+        nombre: "Nangong Yu",
+        imagen: "",
+        atributo: "Eter",
+        especificacion: "Aturdidor",
+        descripcion: "Líder de Angels of Delusion. Encargada de coreografías y baile. Es estable y motivadora.",
+        buildpremium: "",
+        buildF2P: "",
+        sinergias: "Nangong Yu + Aria + Sunna | Nangong Yu + Zhu Yuan + Nicole",
+    },
+    "Aria": {
+        nombre: "Aria",
+        imagen: "",
+        atributo: "Eter",
+        especificacion: "Anomalo",
+        descripcion: "Constructo inteligente. Cantante principal de Angels of Delusion. Nunca se cansa.",
+        buildpremium: "",
+        buildF2P: "",
+        sinergias: "Aria + Nangong Yu + Sunna | Aria + Promeia + Vivian",
+    },
+    "Sunna": {
+        nombre: "Sunna",
+        imagen: "",
+        atributo: "Fisico",
+        especificacion: "Auxiliar",
+        descripcion: "Compositora y letrista principal de Angels of Delusion. Es tímida y reservada.",
+        buildpremium: "",
+        buildF2P: "",
+        sinergias: "Sunna + Nangong Yu + Aria | Sunna + Zhu Yuan + Nicole",
+    },
+
+        // ------------------- DIVISIÓN DE ORDEN METROPOLITANO -------------------
+    "Cissia": {
+        nombre: "Cissia",
+        imagen: "",
+        atributo: "Electrico",
+        especificacion: "Atacante",
+        descripcion: "Especialista en Casos de la División de Orden Metropolitano.[reference:21][reference:22] Es una Thiren serpiente de Rango S.[reference:23][reference:24] Suele ser la primera en ser enviada al campo en cuanto la división obtiene una pista, para su desgracia.[reference:25] Creció en la pobreza en el Anillo Exterior.[reference:26]",
+        buildpremium: "",
+        buildF2P: "",
+        sinergias: "Cissia + Trigger + Rina[reference:27] | Cissia + Seed + Soldier 0 - Anby[reference:28]",
+    },
+
+        // ------------------- PHAETHON -------------------
+    "Pyrois": {
+        nombre: "Pyrois",
+        imagen: "",
+        atributo: "Eter",
+        especificacion: "Atacante",
+        descripcion: "El único agente jugable de la facción Phaethon. Es un agente de Rango S que se obtiene de forma gratuita al avanzar en la historia principal de la versión 3.0.[reference:13][reference:14]",
+        buildpremium: "",
+        buildF2P: "",
+        sinergias: "Pyrois + Norma + Velina | Pyrois + Qingyi + Nicole",
+    },
+    
+
+
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+    const botonesAgentes = document.querySelectorAll(".btn-abrir-modal");
+    const modalElemento = document.getElementById('agenteModal');
+    
+    // Verificamos que el modal exista para evitar errores
+    if (modalElemento) {
+        const modalAgente = new bootstrap.Modal(modalElemento);
+
+        botonesAgentes.forEach(boton => {
+            boton.addEventListener("click", function() {
+                const idAgente = this.getAttribute("data-id-agente");
+                const info = BD_agentes[idAgente]; // Usamos BD_agentes
+
+                if (info) {
+                    document.getElementById("modalNombre").textContent = info.nombre;
+                    // Asegúrate de que las propiedades coincidan con tu objeto BD_agentes
+                    // Nota: En tu archivo tenías 'especificacion', cámbialo a 'rol' si prefieres
+                    document.getElementById("modalAtributo").textContent = `Atributo: ${info.atributo}`;
+                    document.getElementById("modalRol").textContent = `Rol: ${info.especificacion}`;
+                    document.getElementById("modalImagen").src = info.imagen || "";
+                    document.getElementById("modalDescripcion").textContent = info.descripcion;
+                    document.getElementById("modalBuildF2P").textContent = info.buildF2P;
+                    document.getElementById("modalSinergia").textContent = info.sinergias;
+
+                    modalAgente.show();
+                }
+            });
+        });
     }
-
-}
+});
